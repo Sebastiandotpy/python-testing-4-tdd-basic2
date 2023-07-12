8534c6cd9eb660b28ed5a9a33e1975feb53e9243
Sebastian W <117441091+Sebastiandotpy@users.noreply.github.com>
2023-07-12 14:20:32 +0200

    Initial commit

---
+++ src/test.py
@@ -0,0 +1,12 @@
+import unittest
+from app import rnd, max_num_in_list
+
+class TestApp(unittest.TestCase):
+    # check if max_num_in_list will return right value
+    def test_max_num_in_list1(self):
+        self.assertEqual(max_num_in_list([2, 6, 8, 7, -1]), 8, 'return value not the greatest value in max_num_in_list')
+
+
+
+if __name__ == '__main__':
+    unittest.main()
